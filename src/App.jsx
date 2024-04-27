d2d4072ac45ee3f7fd4647a8ab647bcc26572b24
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2024-04-27 22:42:20 +0530

    toaster

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     {/* add toasts container */}
     <div className="toastsContainer">
       <Toaster
-        position="top-right"
+        position="top-center"
         reverseOrder={false}
       />
     </div>
