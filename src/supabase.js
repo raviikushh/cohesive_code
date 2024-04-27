bcb26657800c3d006b1d1c7377ca0d1d85b0a0ca
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2024-04-27 20:24:29 +0530

    added supabase API

--- src/supabase.js
+++ src/supabase.js
@@ -2,8 +2,8 @@
 // setup supabase client 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseKey = 'YOUR_SUPABASE_KEY'
+const supabaseUrl = 'https://xkyovuxsnqmhuzzbzofp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhreW92dXhzbnFtaHV6emJ6b2ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQyMjk1MDIsImV4cCI6MjAyOTgwNTUwMn0.1HKXx3Z-QZZ7iRwnkWDOU_Lq2o6roWmsdrt_4MzqIhU'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
