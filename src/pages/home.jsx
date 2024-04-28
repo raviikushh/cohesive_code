b8aa079d48608744ed18743a355cb7ef0fad69d6
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2024-04-28 08:02:45 +0530

    ui

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -42,35 +42,44 @@ const Home = () => {
 
     return (
         //room id and user name input fields
-        <div className="homePageWrapper flex justify-center">
-            <div className="formWrapper flex flex-col gap-4 items-center justify-center border-2 p-4 mt-10 h-auto border-gray-900 rounded-md bg-slate-600">
-                <h4>Paste invitation ROOM ID</h4>
-                <div className="inputGroup flex flex-col gap-2 items-center">
-                    <input type="text"  className='border-2 border-gray-700  rounded-md p-2 w-52 text-center bg-slate-300'
-                            placeholder="ROOM ID" 
-                            //set room id state on change
-                            onChange={(e) => setRoomId(e.target.value)}
-                            value={roomId}
-                            onKeyUp={handleEnterKey}
+        <div className="homePageWrapper flex justify-center min-h-screen bg-gray-800">
+            <div className="formWrapper flex flex-col gap-8 items-center justify-center border-2 p-8 mt-20 rounded-lg bg-gray-700     shadow-lg">
+                <h4 className="text-2xl font-semibold text-white">Paste invitation ROOM ID</h4>
+                <div className="inputGroup flex flex-col gap-4 items-center">
+                    <input
+                        type="text"
+                        className="border-2 border-gray-600 rounded-md p-3 w-72 text-center bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="ROOM ID"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleEnterKey}
                     />
-                    <input type="text" className='border-2 border-gray-700 rounded-md p-2 w-52 text-center bg-slate-300'
-                            placeholder="USER NAME" 
-                            //set user name state on change
-                            onChange = {(e) => setUserName(e.target.value)}
-                            value={userName} 
-                            onKeyUp={handleEnterKey}
+                    <input
+                        type="text"
+                        className="border-2 border-gray-600 rounded-md p-3 w-72 text-center bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="USER NAME"
+                        onChange={(e) => setUserName(e.target.value)}
+                        value={userName}
+                        onKeyUp={handleEnterKey}
                     />
-                    <button onClick = {JoinRoom} className='border-2 p-1 rounded-md border-slate-700 bg-sky-500 hover:bg-sky-400'>
+                    <button
+                        onClick={JoinRoom}
+                        className="border-2 p-3 rounded-md border-slate-700 bg-blue-600 hover:bg-blue-700 text-white font-semibold w-72 transition-colors duration-300"
+                    >
                         Join Room
                     </button>
-                    <span className=''>
+                    <span className="text-gray-400 text-sm">
                         if you dont have an invite then create &nbsp;
-                        <a className='text-green-400 underline hover:text-sky-500'
-                            onClick={createNewRoom}
-                            href="/editor/123"> new room</a>
+                        <a
+                        className="text-green-400 underline hover:text-blue-500 transition-colors duration-300"
+                        onClick={createNewRoom}
+                        href="/editor/123"
+                        >
+                        new room
+                        </a>
                     </span>
                 </div>
-            </div>
+             </div>
         </div>
     )
 }
